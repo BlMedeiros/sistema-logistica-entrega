8fb08358ad4644e6343a4d57fdfb6e41454f27da
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-11-13 19:58:20 -0300

    Adiciona o handler de cadastrar motorista

--- src/main/java/modules/motorista/application/handlers/CadastrarMotoristaHandler.java
+++ src/main/java/modules/motorista/application/handlers/CadastrarMotoristaHandler.java
@@ -1,4 +1,24 @@
 package modules.motorista.application.handlers;
 
+import infra.eventstore.EventStore;
+import modules.motorista.Motorista;
+import modules.motorista.application.command.CadastrarMotoristaCommand;
+import modules.motorista.domain.event.MotoristaCadastradoEvent;
+import modules.motorista.infra.persistence.dao.MotoristaDAO;
+import modules.motorista.infra.persistence.dao.MotoristaDAOImpl;
+
 public class CadastrarMotoristaHandler {
+
+    public void handle(CadastrarMotoristaCommand cmd) {
+
+        var motorista = new Motorista(cmd.nome(),cmd.cnh(),cmd.veiculo(), cmd.cidadeBase());
+
+        MotoristaDAO dao = new MotoristaDAOImpl();
+
+        dao.cadastrarMotorista(motorista);
+
+        MotoristaCadastradoEvent event = new MotoristaCadastradoEvent(motorista.getId(), motorista.getNome(), motorista.getCnh(), motorista.getVeiculo(), motorista.getCidadeBase());
+
+        EventStore.append(event);
+    }
 }
