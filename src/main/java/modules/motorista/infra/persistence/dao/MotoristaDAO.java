9d16a779a158bbb567ebb179b299abae11b926f1
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-11-13 20:01:30 -0300

    feat: adiciona uma nova interface para o repositorio de motorista

--- src/main/java/modules/motorista/infra/persistence/dao/MotoristaDAO.java
+++ src/main/java/modules/motorista/infra/persistence/dao/MotoristaDAO.java
@@ -1,4 +1,12 @@
 package modules.motorista.infra.persistence.dao;
 
+import modules.motorista.Motorista;
+
 public interface MotoristaDAO {
+
+    void cadastrarMotorista(Motorista motorista);
+
+    Motorista buscarPorId(int id);
+
+    void excluirMotorista(int id);
 }
