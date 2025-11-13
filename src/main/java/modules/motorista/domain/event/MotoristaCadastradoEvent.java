c1d1f92a34679b29c050439537ac00834b49e21c
bruno_l_medeiros <brunoluismedeiros.08@gmail.com>
2025-11-13 20:01:16 -0300

    feat: adiciona um novo event para cliente cadastrado

--- src/main/java/modules/motorista/domain/event/MotoristaCadastradoEvent.java
+++ src/main/java/modules/motorista/domain/event/MotoristaCadastradoEvent.java
@@ -1,4 +1,39 @@
 package modules.motorista.domain.event;
 
-public class MotoristaCadastradoEvent {
+import core.events.Event;
+
+public class MotoristaCadastradoEvent extends Event {
+
+    private final String nome;
+    private final String cnh;
+    private final String veiculo;
+    private final String cidadeBase;
+
+    public MotoristaCadastradoEvent(int aggregateId,
+                                    String nome,
+                                    String cnh,
+                                    String veiculo,
+                                    String cidadeBase) {
+        super(aggregateId);
+        this.nome = nome;
+        this.cnh = cnh;
+        this.veiculo = veiculo;
+        this.cidadeBase = cidadeBase;
+    }
+
+    @Override
+    public String getAggregateType() { return "Motorista"; }
+
+    @Override
+    public String getEventType() { return "MotoristaCadastrado"; }
+
+    @Override
+    public String toJson() {
+        return "{"
+                + "\"nome\":\"" + nome + "\","
+                + "\"cnh\":\"" + cnh + "\","
+                + "\"veiculo\":\"" + veiculo + "\","
+                + "\"cidadeBase\":\"" + cidadeBase + "\""
+                + "}";
+    }
 }
